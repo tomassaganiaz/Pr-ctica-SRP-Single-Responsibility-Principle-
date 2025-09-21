7d164d32d9a66b8fd07dbba61ea76612e3b317b3
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:17:44 -0300

    Update base_procesador.py

--- practicas_SRP/procesadores/base_procesador.py
+++ practicas_SRP/procesadores/base_procesador.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
 
 class BaseProcesador(ABC):
+    """
+    Clase abstracta que define la interfaz para procesadores de datos.
+    """
+
     @abstractmethod
     def procesar(self, datos):
+        """
+        Procesa los datos y retorna un resultado.
+        """
         pass
