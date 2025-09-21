2a450cd3abb589e8924de8adbf95bcc997de1eb3
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:18:40 -0300

    Update procesador_suma.py

--- practicas_SRP/procesadores/procesador_suma.py
+++ practicas_SRP/procesadores/procesador_suma.py
@@ -1,5 +1,9 @@
 from procesadores.base_procesador import BaseProcesador
 
 class ProcesadorSuma(BaseProcesador):
+    """
+    Estrategia concreta: calcula la suma de los datos.
+    """
+
     def procesar(self, datos):
         return sum(datos)
