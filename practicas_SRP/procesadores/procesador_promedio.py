518e98084cca40beb4ab3b4777b1d8c7d7756b50
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:18:13 -0300

    Update procesador_promedio.py

--- practicas_SRP/procesadores/procesador_promedio.py
+++ practicas_SRP/procesadores/procesador_promedio.py
@@ -1,5 +1,9 @@
 from procesadores.base_procesador import BaseProcesador
 
 class ProcesadorPromedio(BaseProcesador):
+    """
+    Estrategia concreta: calcula el promedio de los datos.
+    """
+
     def procesar(self, datos):
-        return sum(datos) / len(datos)
+        return sum(datos) / len(datos) if datos else 0
