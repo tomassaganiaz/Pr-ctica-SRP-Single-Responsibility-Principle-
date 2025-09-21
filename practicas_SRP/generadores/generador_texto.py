7e6f4f95fdbe577aaef1d6b9567a4e5b60b3448f
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:19:49 -0300

    Update generador_texto.py

--- practicas_SRP/generadores/generador_texto.py
+++ practicas_SRP/generadores/generador_texto.py
@@ -1,3 +1,7 @@
 class GeneradorTexto:
+    """
+    Genera la salida del reporte en formato de texto plano.
+    """
+
     def generar(self, resultado):
         return f"El resultado es: {resultado}"
