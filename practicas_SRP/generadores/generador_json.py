6265b6a04c487d088209d37fed65af08869303da
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:20:08 -0300

    Update generador_json.py

--- practicas_SRP/generadores/generador_json.py
+++ practicas_SRP/generadores/generador_json.py
@@ -1,5 +1,9 @@
 import json
 
 class GeneradorJSON:
+    """
+    Genera la salida del reporte en formato JSON.
+    """
+
     def generar(self, resultado):
         return json.dumps({"resultado": resultado}, indent=2)
