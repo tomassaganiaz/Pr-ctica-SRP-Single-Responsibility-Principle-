709c1c7117a477bc7cc0e82a07848a478fd936ea
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:28:05 -0300

    Update test_reporte.py

--- practicas_SRP/tests/test_reporte.py
+++ practicas_SRP/tests/test_reporte.py
@@ -1,1 +1,20 @@
+from fuentes.fuente_datos import FuenteDatos
+from procesadores.procesador_promedio import ProcesadorPromedio
+from generadores.generador_texto import GeneradorTexto
+from entregadores.mostrar_consola import MostrarConsola
+from reporte.reporte import Reporte
 
+def test_reporte_ejecutar(capfd):
+    """
+    Test de integración: ejecuta el flujo completo y captura salida en consola.
+    """
+    fuente = FuenteDatos()
+    procesador = ProcesadorPromedio()
+    generador = GeneradorTexto()
+    entregador = MostrarConsola()
+
+    reporte = Reporte(fuente, procesador, generador, entregador)
+    reporte.ejecutar()
+
+    out, err = capfd.readouterr()
+    assert "El resultado es:" in out
