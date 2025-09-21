8697266965cd456eff3ef5b42297cec26df5afc9
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:19:07 -0300

    Update procesador_maximo.py

--- practicas_SRP/procesadores/procesador_maximo.py
+++ practicas_SRP/procesadores/procesador_maximo.py
@@ -1,5 +1,9 @@
 from procesadores.base_procesador import BaseProcesador
 
 class ProcesadorMaximo(BaseProcesador):
+    """
+    Estrategia concreta: obtiene el valor máximo de los datos.
+    """
+
     def procesar(self, datos):
-        return max(datos)
+        return max(datos) if datos else None
