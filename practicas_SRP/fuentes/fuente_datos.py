dd622cf321a6cf84c5fb7ff6525684fe90a9dd4f
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-21 18:17:07 -0300

    Update fuente_datos.py

--- practicas_SRP/fuentes/fuente_datos.py
+++ practicas_SRP/fuentes/fuente_datos.py
@@ -1,3 +1,11 @@
 class FuenteDatos:
-    def obtener(self):
+    """
+    Clase que representa la fuente de datos.
+    Obtiene los datos desde una lista fija (ejemplo).
+    """
+
+    def obtener_datos(self):
+        """
+        Retorna una lista de datos de ejemplo.
+        """
         return [10, 20, 30, 40, 50]
